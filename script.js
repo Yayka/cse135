ee9ff96b38b845a6c42ac67ab6afb9cefc0d45cb
ntrappe <ntrappe@ucsd.edu>
2021-04-05 14:58:58 -0700

    initial index.html file

---
+++ script.js
@@ -0,0 +1,40 @@
+// script.js
+
+/* get all assignments in list */
+const assignment1 = document.getElementById('assignment1');
+const assignment2 = document.getElementById('assignment2');
+const assignment3 = document.getElementById('assignment3');
+
+/* get all team members */
+const diana = document.getElementById('diana');
+const nicole = document.getElementById('nicole');
+
+/* current site link (home url) */
+const link = window.location.href;
+console.log(link);
+
+/**
+ * Add mouse click event to each assignment including text content
+ * and the div container to act like a link
+ */
+assignment1.addEventListener('click', () => {
+  console.log('in assignment1!');
+});
+assignment2.addEventListener('click', () => {
+  console.log('in assignment2!');
+});
+assignment3.addEventListener('click', () => {
+  console.log('in assignment3!');
+});
+
+/**
+ * Add mouse click event to each team name to act as a link
+ */
+diana.addEventListener('click', () => {
+  console.log('clicked diana!');
+  window.location.href = 'http://127.0.0.1:5500/diana.html';
+});
+nicole.addEventListener('click', () => {
+  console.log('clicked nicole!');
+  window.location.href = 'http://127.0.0.1:5500/nicole.html';
+});
